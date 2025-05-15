71a53740c2c21e420410139d9ddd2a30a1e0db3b
bhanu kiran <bhanukiran33@gmail.com>
2025-05-15 19:29:22 +0530

    shell updated

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: Hey Musk, How are you?"
+echo "$PERSON2:: Hello Trump, I am fine. How are you doing"
+echo "$PERSON1: I am fine too. What's up?"
+echo "$PERSON2:: Nohting, just going to Mars now, will you come?"
+echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
