033e713f5bb56cd0305a265aa96d55fcf008826c
bhanu kiran <bhanukiran33@gmail.com>
2025-05-16 11:31:35 +0530

    shell updated

---
+++ 07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "First Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
+echo "first Movie: ${MOVIES[2]}"
+
+echo "All movies: ${MOVIES[@]}"
---
+++ 08-special-vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "All variables passed the script   $@"
+echo "Number of variables: $#"
+echo "script name : $0"
+echo "Current Directory : $PWD"
