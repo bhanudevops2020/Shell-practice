c9d3660253d407e2e9d1d354d70b20cf44544c4d
bhanukiran <bhanudevops2020@gmail.com>
2025-07-17 22:37:47 +0530

    updated shell

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,5 +2,10 @@
 
 echo "All variables passed the script   $@"
 echo "Number of variables: $#"
-# echo "script name : $0"
-# echo "Current Directory : $PWD"
+echo "script name : $0"
+echo "Current Directory: $PWD"
+echo "user running this script: $USER"
+echo "Home Directory of user: $HOME"
+echo "Home directory of user: $$"
+sleep 10 &
+echo "Pid of last comamnd in background: $!"
